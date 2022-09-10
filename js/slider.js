1e17c40a921024936721622e3bc143dff80de498
Matías Gutiérrez <ma.gutierrez.or@gmail.com>
2022-09-10 14:31:39 -0400

    damn mime type or colons

--- js/slider.js
+++ js/slider.js
@@ -1,7 +1,27 @@
-(function() {
+(function(){
   
   const sliders = [...document.querySelectorAll('.testimony__body')];
+  const buttonNext = document.querySelector('#next')
+  const buttonBefore = document.querySelector('#before')
+  let value
 
-  console.log(sliders)
+  buttonNext.addEventListener('click', ev => {
+    changePosition(1)
+  })
 
-})()
+  buttonBefore.addEventListener('click', ev => {
+    changePosition(-1)
+  })
+
+  const changePosition = (add) => {
+    const currentTestimony = document.querySelector('.testimony__body--show').dataset.id;
+    value = Number(currentTestimony);
+    value += add;
+    sliders[Number(currentTestimony)-1].classList.remove('testimony__body--show');
+    if (value === sliders.length+1 || value === 0) {
+      value = value === 0 ? sliders.length : 1;
+    }
+    sliders[value-1].classList.add('testimony__body--show');
+
+  }
+})();
